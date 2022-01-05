6c76064e176f71bf13ee371985140bed33224feb
Hamza <e1853813@cmaisonneuve.qc.ca>
2022-01-05 16:49:37 -0500

    TODO app

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>What is the plan?</h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
